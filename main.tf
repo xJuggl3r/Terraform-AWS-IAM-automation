d154ce48da992efcfe735e52dd15bf9a75cddc25
xJuggl3r <xJuggl3r@gmail.com>
2021-05-15 22:45:47 -0400

    original .sh

--- main.tf
+++ main.tf
@@ -86,6 +86,6 @@ module "aws-enforce-mfa" {
 # Cria users e senhas from .csv, add to groups
 resource "null_resource" "Add-users-pass" {
  provisioner "local-exec" {
-    command = "/bin/bash chmod +x ./cria-users.sh || ./cria-users.sh"
+    command = "/bin/bash chmod +x ./cria-users.sh || ./aws-iam-cria-usuario.sh usuarios2.csv"
   }
 }
